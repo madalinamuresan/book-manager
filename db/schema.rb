8d05f031531e1020b6914171798f59f302388e4f
madalina <madalina.sandulescu@hotmail.com>
2020-05-12 15:56:25 +0300

    Added controller actions , set defaulf route, added bulma, pry gems , added new book view, index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_140047) do
+ActiveRecord::Schema.define(version: 2020_05_08_113733) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_140047) do
     t.integer "year"
     t.integer "rating"
     t.string "status"
+    t.string "image_url"
   end
 
 end
